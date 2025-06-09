e31e689423f93d9d79029771636ade9d84e8aebd
amayahbaig786 <amayahbaig30@gmail.com>
2025-06-09 17:58:45 +0100

    Add files via upload

--- masking.py
+++ masking.py
@@ -26,3 +26,34 @@ while True :
     lower_red = np.array([100,40,40])
     upper_red = np.array([100,255,255])
     
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+    lower_red = np.array([155,40,40])
+    upper_red = np.array([180,255,255])
+
+    mask2 = cv2.inRange(hsv, lower_red, upper_red)
+
+    mask1 = mask1+mask2
+
+    #refining the images
+    
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3,3),np.uint8),iterations = 2)
+    mask1 = cv2.dilate(mask1,np.ones((3,3),np.uint8),iterations = 1)
+    mask2 = cv2.bitwise_not(mask1)
+
+    #masking happens here
+
+    result1 = cv2.bitwise_and(background,background,mask=mask1)
+    result2 = cv2.bitwise_and(img,img,mask=mask2)
+    final_output = cv2.addWeighted(result1,1,result2,1,0)
+    cv2.imshow("camera",final_output)
+    k = cv2.waitKey(10)
+    if k == 27:
+        break
+
+video.release()
+cv2.destroyAllWindows()
+
+
+
+  
